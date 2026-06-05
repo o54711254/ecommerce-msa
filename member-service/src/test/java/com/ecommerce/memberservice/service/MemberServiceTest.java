b72e0cc681ad01a6b6d0ec767f43073b6562e849
오건영 <o54711254@gmail.com>
2026-06-05 18:07:17 +0900

    test: member-service getMemberProfile 단위테스트 추가

--- member-service/src/test/java/com/ecommerce/memberservice/service/MemberServiceTest.java
+++ member-service/src/test/java/com/ecommerce/memberservice/service/MemberServiceTest.java
@@ -3,6 +3,7 @@ package com.ecommerce.memberservice.service;
 import com.ecommerce.memberservice.dto.req.JoinMemberRequest;
 import com.ecommerce.memberservice.dto.req.LoginRequest;
 import com.ecommerce.memberservice.dto.res.LoginResponse;
+import com.ecommerce.memberservice.dto.res.MemberProfileResponse;
 import com.ecommerce.memberservice.dto.res.MemberResponse;
 import com.ecommerce.memberservice.entity.Member;
 import com.ecommerce.memberservice.entity.Role;
@@ -11,6 +12,8 @@ import com.ecommerce.memberservice.global.exception.custom.InvalidPasswordExcept
 import com.ecommerce.memberservice.global.exception.custom.MemberNotFoundException;
 import com.ecommerce.memberservice.repository.MemberRepository;
 import com.ecommerce.memberservice.util.JwtUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -28,95 +31,125 @@ import static org.mockito.BDDMockito.given;
 @ExtendWith(MockitoExtension.class)
 class MemberServiceTest {
 
-    @InjectMocks
-    private MemberService memberService;
+    @InjectMocks private MemberService memberService;
+    @Mock private MemberRepository memberRepository;
+    @Mock private BCryptPasswordEncoder passwordEncoder;
+    @Mock private JwtUtil jwtUtil;
 
-    @Mock
-    private MemberRepository memberRepository;
+    @Nested
+    @DisplayName("join - 회원가입")
+    class JoinTest {
 
-    @Mock
-    private BCryptPasswordEncoder passwordEncoder;
+        @Test
+        void 일반_회원_가입_성공() {
+            JoinMemberRequest request = new JoinMemberRequest("test@test.com", "홍길동", "1234", "서울");
+            Member savedMember = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
 
-    @Mock
-    private JwtUtil jwtUtil;
+            given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.empty());
+            given(passwordEncoder.encode("1234")).willReturn("encoded");
+            given(memberRepository.save(any(Member.class))).willReturn(savedMember);
 
-    @Test
-    void 회원가입_성공() {
-        JoinMemberRequest request = new JoinMemberRequest("test@test.com", "홍길동", "1234", "서울");
-        Member savedMember = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+            MemberResponse response = memberService.join(request, Role.MEMBER);
 
-        given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.empty());
-        given(passwordEncoder.encode("1234")).willReturn("encoded");
-        given(memberRepository.save(any(Member.class))).willReturn(savedMember);
+            assertThat(response.email()).isEqualTo("test@test.com");
+            assertThat(response.role()).isEqualTo(Role.MEMBER);
+        }
 
-        MemberResponse response = memberService.join(request, Role.MEMBER);
+        @Test
+        void 판매자_가입_성공() {
+            JoinMemberRequest request = new JoinMemberRequest("seller@test.com", "판매자", "1234", "부산");
+            Member savedMember = new Member("seller@test.com", Role.SELLER, "판매자", "encoded", "부산");
 
-        assertThat(response.email()).isEqualTo("test@test.com");
-        assertThat(response.role()).isEqualTo(Role.MEMBER);
+            given(memberRepository.findByEmail("seller@test.com")).willReturn(Optional.empty());
+            given(passwordEncoder.encode("1234")).willReturn("encoded");
+            given(memberRepository.save(any(Member.class))).willReturn(savedMember);
+
+            MemberResponse response = memberService.join(request, Role.SELLER);
+
+            assertThat(response.email()).isEqualTo("seller@test.com");
+            assertThat(response.role()).isEqualTo(Role.SELLER);
+        }
+
+        @Test
+        void 실패_이메일_중복() {
+            JoinMemberRequest request = new JoinMemberRequest("test@test.com", "홍길동", "1234", "서울");
+            Member existing = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+
+            given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(existing));
+
+            assertThatThrownBy(() -> memberService.join(request, Role.MEMBER))
+                    .isInstanceOf(DuplicateEmailException.class)
+                    .hasMessageContaining("이미 사용 중인 이메일입니다");
+        }
     }
 
-    @Test
-    void 판매자_회원가입_성공() {
-        JoinMemberRequest request = new JoinMemberRequest("seller@test.com", "판매자", "1234", "부산");
-        Member savedMember = new Member("seller@test.com", Role.SELLER, "판매자", "encoded", "부산");
+    @Nested
+    @DisplayName("login - 로그인")
+    class LoginTest {
 
-        given(memberRepository.findByEmail("seller@test.com")).willReturn(Optional.empty());
-        given(passwordEncoder.encode("1234")).willReturn("encoded");
-        given(memberRepository.save(any(Member.class))).willReturn(savedMember);
+        @Test
+        void 성공() {
+            LoginRequest request = new LoginRequest("test@test.com", "1234");
+            Member member = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
 
-        MemberResponse response = memberService.join(request, Role.SELLER);
+            given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(member));
+            given(passwordEncoder.matches("1234", "encoded")).willReturn(true);
+            given(jwtUtil.generateToken(any(), any())).willReturn("jwt-token");
 
-        assertThat(response.email()).isEqualTo("seller@test.com");
-        assertThat(response.role()).isEqualTo(Role.SELLER);
+            LoginResponse response = memberService.login(request);
+
+            assertThat(response.token()).isEqualTo("jwt-token");
+        }
+
+        @Test
+        void 실패_이메일_없음() {
+            LoginRequest request = new LoginRequest("none@test.com", "1234");
+
+            given(memberRepository.findByEmail("none@test.com")).willReturn(Optional.empty());
+
+            assertThatThrownBy(() -> memberService.login(request))
+                    .isInstanceOf(MemberNotFoundException.class)
+                    .hasMessageContaining("회원을 찾을 수 없습니다");
+        }
+
+        @Test
+        void 실패_비밀번호_불일치() {
+            LoginRequest request = new LoginRequest("test@test.com", "wrong");
+            Member member = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+
+            given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(member));
+            given(passwordEncoder.matches("wrong", "encoded")).willReturn(false);
+
+            assertThatThrownBy(() -> memberService.login(request))
+                    .isInstanceOf(InvalidPasswordException.class)
+                    .hasMessageContaining("비밀번호가 일치하지 않습니다");
+        }
     }
 
-    @Test
-    void 회원가입_이메일_중복() {
-        JoinMemberRequest request = new JoinMemberRequest("test@test.com", "홍길동", "1234", "서울");
-        Member existing = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+    @Nested
+    @DisplayName("getMemberProfile - 프로필 조회")
+    class GetMemberProfileTest {
 
-        given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(existing));
+        @Test
+        void 성공() {
+            Long memberId = 1L;
+            Member member = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+            given(memberRepository.findById(memberId)).willReturn(Optional.of(member));
 
-        assertThatThrownBy(() -> memberService.join(request, Role.MEMBER))
-                .isInstanceOf(DuplicateEmailException.class)
-                .hasMessageContaining("이미 사용 중인 이메일입니다");
-    }
+            MemberProfileResponse response = memberService.getMemberProfile(memberId);
 
-    @Test
-    void 로그인_성공() {
-        LoginRequest request = new LoginRequest("test@test.com", "1234");
-        Member member = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
+            assertThat(response.email()).isEqualTo("test@test.com");
+            assertThat(response.role()).isEqualTo(Role.MEMBER);
+            assertThat(response.name()).isEqualTo("홍길동");
+            assertThat(response.address()).isEqualTo("서울");
+        }
 
-        given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(member));
-        given(passwordEncoder.matches("1234", "encoded")).willReturn(true);
-        given(jwtUtil.generateToken(any(), any())).willReturn("jwt-token");
+        @Test
+        void 실패_회원_없음() {
+            given(memberRepository.findById(any())).willReturn(Optional.empty());
 
-        LoginResponse response = memberService.login(request);
-
-        assertThat(response.token()).isEqualTo("jwt-token");
-    }
-
-    @Test
-    void 로그인_이메일_없음() {
-        LoginRequest request = new LoginRequest("none@test.com", "1234");
-
-        given(memberRepository.findByEmail("none@test.com")).willReturn(Optional.empty());
-
-        assertThatThrownBy(() -> memberService.login(request))
-                .isInstanceOf(MemberNotFoundException.class)
-                .hasMessageContaining("회원을 찾을 수 없습니다");
-    }
-
-    @Test
-    void 로그인_비밀번호_불일치() {
-        LoginRequest request = new LoginRequest("test@test.com", "wrong");
-        Member member = new Member("test@test.com", Role.MEMBER, "홍길동", "encoded", "서울");
-
-        given(memberRepository.findByEmail("test@test.com")).willReturn(Optional.of(member));
-        given(passwordEncoder.matches("wrong", "encoded")).willReturn(false);
-
-        assertThatThrownBy(() -> memberService.login(request))
-                .isInstanceOf(InvalidPasswordException.class)
-                .hasMessageContaining("비밀번호가 일치하지 않습니다");
+            assertThatThrownBy(() -> memberService.getMemberProfile(999L))
+                    .isInstanceOf(MemberNotFoundException.class);
+        }
     }
 }
